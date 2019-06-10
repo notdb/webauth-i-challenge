34557b01dc823acde131fd0de012e72e5375025b
Doug <me@douglasbowen.com>
2019-06-10 19:01:56 -0400

    add middleware

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+// require bcrypt
+const bcrypt = require("bcryptjs");
+
+const db = require("./database/dbConfig.js");
+const Users = require("./users/users-model.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send("It's alive!");
+});
+
+server.post("/api/register", (req, res) => {
+  let user = req.body;
+  // hash the password
+  const hash = bcrypt.hashSync(user.password, 16); // password gets re-hashed 2^16 times
+  // re-assign user password
+  user.password = hash;
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.post("/api/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user) {
+        res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.get("/api/users", restricted, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(400).json({ message: "please enter a username and password" });
+  }
+}
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
