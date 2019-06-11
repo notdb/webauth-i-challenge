f66dce9e753b7a9cf7bd6d86f7e03515b9bbf7b9
Doug <me@douglasbowen.com>
2019-06-11 18:31:04 -0400

    add sessions

--- index.js
+++ index.js
@@ -5,14 +5,37 @@ const cors = require("cors");
 // require bcrypt
 const bcrypt = require("bcryptjs");
 
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+
 const db = require("./database/dbConfig.js");
 const Users = require("./users/users-model.js");
 
 const server = express();
 
+const sessionConfig = {
+  name: "monkey1",
+  secret: "keep it secret, keep it safe!",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true
+  },
+  store: new KnexSessionStore({
+    knex: require("./database/dbConfig.js"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    createInterval: (1000 * 60) & 30
+  })
+};
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.get("/", (req, res) => {
   res.send("It's alive!");
@@ -21,7 +44,7 @@ server.get("/", (req, res) => {
 server.post("/api/register", (req, res) => {
   let user = req.body;
   // hash the password
-  const hash = bcrypt.hashSync(user.password, 16); // password gets re-hashed 2^16 times
+  const hash = bcrypt.hashSync(user.password, 8); // password gets re-hashed 2^8 times
   // re-assign user password
   user.password = hash;
   Users.add(user)
@@ -39,7 +62,8 @@ server.post("/api/login", (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.username = user.username;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
@@ -53,7 +77,7 @@ server.post("/api/login", (req, res) => {
 server.get("/api/users", restricted, (req, res) => {
   Users.find()
     .then(users => {
-      res.json(users);
+      res.json({ users, username: req.session.username });
     })
     .catch(err => res.send(err));
 });
